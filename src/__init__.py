a967f236b7bee8a651164ad2a11758eaa7d8c57d
Elli <nathan@cypherlab.org>
2025-05-21 10:13:25 +0200

    init

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-# Change to this to fix the F401 errors:
-from .add import add, subtract  # noqa: F401
+from .add import add, subtract
 
+# Define what gets imported with "from package import *"
+__all__ = ["add", "subtract"]
 __version__ = "0.1.0"
