0ebac5d92c1c74755a13e6ab6c1d2534eaf5d233
Elli <nathan@cypherlab.org>
2025-05-21 14:48:23 +0200

    bump version

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ from .call_encoder import encode_call
 
 # Define what gets imported with "from package import *"
 __all__ = ["add", "subtract", "encode_call"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
